268f87dc08f49efb708bf636080863a23d7b66ca
Nattawut Singhchai <jadedragon17650@gmail.com>
2017-01-12 12:13:25 +0700

    Initial Commit

---
+++ Pot Monitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pot Monitor
+//
+//  Created by Nattawut Singhchai on 1/12/17.
+//  Copyright © 2017 Nattawut Singhchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
